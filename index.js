110163e93f3e57d3487e696b9b5db2d63d77f8f5
Lena Hou <lenahou2013@gmail.com>
2021-10-01 20:03:17 -0400

    commit tree 5d908e41b9cfcac4e7e4fe011cebde950032adbe

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+   return drivers.filter(find_string => {
+       return(find_string.toLowerCase() === string.toLowerCase())
+   })
+}
+
+function fuzzyMatch(drivers, letter) {
+  return drivers.filter( find_string => {
+      if (letter.charAt(0) === find_string.charAt(0)) {
+         return find_string
+      }
+  })
+}
+
+function matchName(drivers, string) {
+   return drivers.filter( function(find_string){
+       if (find_string.name == string) {
+           return find_string
+       }
+   })
+}
